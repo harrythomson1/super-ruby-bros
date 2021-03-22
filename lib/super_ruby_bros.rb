d8d9b9d3ae0da06c790805c69066adf4f1c67de5
Harry Thomson <haroldt95@hotmail.com>
2021-03-22 17:41:56 +0000

    Added gravity and started a jump function

--- lib/super_ruby_bros.rb
+++ lib/super_ruby_bros.rb
@@ -1,11 +1,19 @@
 require 'ruby2d'
 
 set title: "Super Ruby Bros", background: 'red'
+FLOOR = 400
 
-@square = Square.new(x: 40, y: 70, size: 25, color: 'blue')
+@square = Square.new(x: 40, y: FLOOR, size: 25, color: 'blue')
 
 @x_speed = 0
 @y_speed = 0
+
+
+tick = 0
+
+def jump
+  @square.y += -5
+end
 
 on :key do |event|
   if event.key == 'a'
@@ -14,12 +22,11 @@ on :key do |event|
   elsif event.key == 'd'
     @x_speed = 2
     @y_speed = 0
-  elsif event.key == 'w'
-    @x_speed = 0
-    @y_speed = -2
-  elsif event.key == 's'
-    @x_speed = 0
-    @y_speed = 2
+  # elsif event.key == 'j'
+  #   if @square.y == FLOOR
+  #     @square.y -= 75
+  #     p @square.y
+  #   end
   end
 end
 
@@ -30,18 +37,25 @@ on :key_up do |event|
   elsif event.key == 'd'
     @x_speed = 0
     @y_speed = 0
-  elsif event.key == 'w'
-    @x_speed = 0
-    @y_speed = 0
-  elsif event.key == 's'
-    @x_speed = 0
-    @y_speed = 0
   end
 end
 
 update do
   @square.x += @x_speed
   @square.y += @y_speed
+
+  if tick % 2 == 0 && @square.y < FLOOR
+    @square.y += 5
+  end
+
+  tick += 1
+
+  on :key do |event|
+    if event.key == 'j' && @square.y > 325
+      @square.y -= 0.03
+    end
+  end
+
 end
 
 show
