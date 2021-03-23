272ecc37f92e9f3e7321b5f0eaeaf8c1f4e54fff
Harry Thomson <haroldt95@hotmail.com>
2021-03-23 12:13:41 +0000

    The square now consitently jumps and has gravity

--- lib/super_ruby_bros.rb
+++ lib/super_ruby_bros.rb
@@ -3,6 +3,8 @@ require 'ruby2d'
 set title: "Super Ruby Bros", background: 'red'
 FLOOR = 400
 CEILING = 325
+GRAVITY = 2
+@jumper_state = "ready"
 
 @square = Square.new(x: 40, y: FLOOR, size: 25, color: 'blue')
 
@@ -13,31 +15,24 @@ CEILING = 325
 tick = 0
 
 def jump
-  @square.y -= 75
+  if @jumper_state == "ready"
+    @y_speed = -10
+  end
 end
 
 on :key do |event|
   if event.key == 'a'
     @x_speed = -2
-    @y_speed = 0
   elsif event.key == 'd'
     @x_speed = 2
-    @y_speed = 0
-  # This code will create a jump but the animation won't look great  
-  elsif event.key == 'space'
-    if @square.y == FLOOR
-      jump
-    end
   end
 end
 
 on :key_up do |event|
   if event.key == 'a'
     @x_speed = 0
-    @y_speed = 0
   elsif event.key == 'd'
     @x_speed = 0
-    @y_speed = 0
   end
 end
 
@@ -45,17 +40,26 @@ update do
   @square.x += @x_speed
   @square.y += @y_speed
 
-  if tick % 2 == 0 && @square.y < FLOOR
-    @square.y += 5
-  end
-
   tick += 1
 
-  # on :key do |event|
-  #   if event.key == 'space' && @square.y > CEILING
-  #     @square.y -= 0.03
-  #   end
-  # end
+  on :key do |event|
+    if event.key == 'space' && @square.y > CEILING
+      jump
+    end
+  end
+
+  if @square.y < (CEILING)
+    @jumper_state = 'jumping'  
+  end
+
+  if @jumper_state == 'jumping'
+    @y_speed = GRAVITY
+  end
+
+  if @square.y == FLOOR
+    @jumper_state = 'ready'
+    @y_speed = 0
+  end
 
 end
 
